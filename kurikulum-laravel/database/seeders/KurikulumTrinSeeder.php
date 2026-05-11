a4f9ad57dc798bc3ed38a9cb175349caf2b36333
Mobocat <ahmatbriansyach@gmail.com>
2026-05-11 09:45:16 +0700

    gk jadi

--- kurikulum-laravel/database/seeders/KurikulumTrinSeeder.php
+++ kurikulum-laravel/database/seeders/KurikulumTrinSeeder.php
@@ -9,27 +9,13 @@ class KurikulumTrinSeeder extends Seeder
 {
     public function run(): void
     {
-        // Seed Roles
-        DB::table('roles')->insert([
-            ['id' => 1, 'name' => 'Kaprodi', 'guard_name' => 'web', 'created_at' => '2026-05-08 23:04:43', 'updated_at' => '2026-05-08 23:04:43'],
-            ['id' => 2, 'name' => 'Dosen', 'guard_name' => 'web', 'created_at' => '2026-05-08 23:04:43', 'updated_at' => '2026-05-08 23:04:43'],
-        ]);
+
 
         // Seed Users
-        DB::table('users')->insert([
-            ['id' => 1, 'dosen_biodata_id' => null, 'name' => 'Polisi TRIN', 'email' => 'kaprodi@polman.edu', 'password' => '$2y$12$XFox44rIHcjfXJH8t2HVjOrUjLVmVN.1WNHVtDNOjurqIAD9LE4v6', 'created_at' => '2026-05-08 23:04:43', 'updated_at' => '2026-05-08 23:04:43', 'nip' => '198001012005011001', 'jabatan' => null],
-            ['id' => 2, 'dosen_biodata_id' => 1, 'name' => 'tessss', 'email' => 'admin@ae.ac.id', 'password' => '$2y$12$jemKNIa8HSzLKTM3zV.jmuO5NLoUdhIIfV7O3lQGYiQ/F9TfxP3FK', 'created_at' => '2026-05-08 23:04:50', 'updated_at' => '2026-05-08 23:04:50', 'nip' => null, 'jabatan' => null],
-            ['id' => 3, 'dosen_biodata_id' => 8, 'name' => 'Siti Aminah', 'email' => 'siti@ae.polman-bandung.ac.id', 'password' => '$2y$12$pWTCPSuAFePFsOrBvX7jMeGnhnLYBpy/OzhE8hOY2Xhjiw7YXXPBq', 'created_at' => '2026-05-09 21:27:12', 'updated_at' => '2026-05-09 21:27:12', 'nip' => '197408172009122001', 'jabatan' => null],
-            ['id' => 4, 'dosen_biodata_id' => 11, 'name' => 'Ridwan', 'email' => 'ridwan@ae.polman-bandung.ac.id', 'password' => '$2y$12$XW3nICcoj.pa7Nl3/wxMQOjn/6zqYAqhG7yabfA8SA5Wn1UhIx5My', 'created_at' => '2026-05-09 21:34:45', 'updated_at' => '2026-05-09 21:34:45', 'nip' => '197806122001121002', 'jabatan' => null],
-        ]);
+
 
         // Seed Model Has Roles
-        DB::table('model_has_roles')->insert([
-            ['role_id' => 1, 'model_type' => 'App\\Models\\User', 'model_id' => 1],
-            ['role_id' => 1, 'model_type' => 'App\\Models\\User', 'model_id' => 2],
-            ['role_id' => 1, 'model_type' => 'App\\Models\\User', 'model_id' => 3],
-            ['role_id' => 1, 'model_type' => 'App\\Models\\User', 'model_id' => 4],
-        ]);
+
 
         // Seed CPLs
         DB::table('cpls')->insert([
@@ -62,9 +48,6 @@ class KurikulumTrinSeeder extends Seeder
             ['id' => 1, 'kode_mk' => '1232144ew', 'nama_mk' => 'matematika', 'sks' => 2, 'jenis' => 'Teori', 'deskripsi' => 'awdawdwadw', 'semester' => '1', 'sifat_pengambilan' => 'Wajib', 'cara_pembelajaran' => 'Daring', 'created_at' => '2026-05-08 23:49:26', 'updated_at' => '2026-05-08 23:49:26', 'prasyarat_id' => null]
         ]);
 
-        // Seed Dosen Biodata Mata Kuliah
-        DB::table('dosen_biodata_mata_kuliah')->insert([
-            ['id' => 1, 'dosen_biodata_id' => 2, 'mata_kuliah_id' => 1, 'created_at' => '2026-05-09 01:07:15', 'updated_at' => '2026-05-09 01:07:15']
-        ]);
+
     }
 }
